f19ab4c7f7d06705edad8f8296136079bc403ab9
Jeeseob <jeeseob5761@gmail.com>
2022-03-28 15:17:46 +0900

    create

---
+++ src/main/java/com/graduatioinProject/sensorMonitoring/SensorMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.graduatioinProject.sensorMonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorMonitoringApplication.class, args);
+	}
+
+}
