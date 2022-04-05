c9591181ab244a390e728e2d890905517e4a3915
Jeeseob <jeeseob5761@gmail.com>
2022-04-05 22:35:33 +0900

    api response 테스트

--- src/main/java/com/graduatioinProject/sensorMonitoring/node/formerData/temperature/TemperatureDataController.java
+++ src/main/java/com/graduatioinProject/sensorMonitoring/node/formerData/temperature/TemperatureDataController.java
@@ -2,9 +2,10 @@ package com.graduatioinProject.sensorMonitoring.node.formerData.temperature;
 
 import com.graduatioinProject.sensorMonitoring.baseUtil.dto.ListResult;
 import com.graduatioinProject.sensorMonitoring.baseUtil.dto.SingleResult;
+import com.graduatioinProject.sensorMonitoring.baseUtil.exception.BussinessException;
 import com.graduatioinProject.sensorMonitoring.baseUtil.service.ResponseService;
 
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -12,28 +13,38 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @RestController
-@RequestMapping("/formerData")
+@RequiredArgsConstructor
+@RequestMapping("api/formerData")
 public class TemperatureDataController {
 
     private final TemperatureDataService temperatureDataService;
     private final ResponseService responseService;
 
-    public TemperatureDataController(TemperatureDataService formerDataService, ResponseService responseService) {
-        this.temperatureDataService = formerDataService;
-        this.responseService = responseService;
-    }
-
     @GetMapping("/temperatureList")
     public ListResult<TemperatureData> getTemperatureList(@RequestBody HashMap<String, Object> body) {
         LocalDate start = LocalDate.parse((String) body.get("startDate"), DateTimeFormatter.ISO_DATE);
         LocalDate end = LocalDate.parse((String) body.get("endDate"), DateTimeFormatter.ISO_DATE);
-        List<TemperatureData> temperatureDataArrayList = temperatureDataService.findTemperatures(start, end);
-
-        if(temperatureDataArrayList.isEmpty()) {
-            return (ListResult<TemperatureData>) responseService.failResult("해당 날짜에 대한 정보가 없습니다.");
+        try {
+            return responseService.listResult(
+                    temperatureDataService.findTemperatures(start,end));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BussinessException(e.getMessage());
         }
-        return responseService.listResult(temperatureDataArrayList);
     }
+
+    @GetMapping("/temperature")
+    public SingleResult<TemperatureData> getTemperature(@RequestBody HashMap<String, Object> body) {
+        LocalDate date = LocalDate.parse((String) body.get("date"), DateTimeFormatter.ISO_DATE);
+        try {
+            return responseService.singleResult(
+                    temperatureDataService.findTemperature(date).get());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new BussinessException(e.getMessage());
+        }
+    }
+
 
     /**
      * Test
@@ -43,7 +54,7 @@ public class TemperatureDataController {
 //    public ResponseEntity<ListResult<TemperatureData>> getTemperatureList(@RequestBody HashMap<String, Object> body) {
 //        LocalDate start = LocalDate.parse((String) body.get("startDate"), DateTimeFormatter.ISO_DATE);
 //        LocalDate end = LocalDate.parse((String) body.get("endDate"), DateTimeFormatter.ISO_DATE);
-//        List<TemperatureData> temperatureDataArrayList = formerDataService.findTemperatures(start, end);
+//        List<TemperatureData> temperatureDataArrayList = temperatureDataService.findTemperatures(start, end);
 //
 //        if(temperatureDataArrayList.isEmpty()) {
 //            return ResponseEntity.ok((ListResult<TemperatureData>) responseService.failResult("해당 날짜에 대한 정보가 없습니다."));
@@ -57,15 +68,5 @@ public class TemperatureDataController {
         LocalDate end = LocalDate.parse((String) body.get("endDate"), DateTimeFormatter.ISO_DATE);
         List<TemperatureData> temperatureDataArrayList = temperatureDataService.findTemperatures(start, end);
         return temperatureDataArrayList;
-    }
-
-    @GetMapping("/temperature")
-    public SingleResult<TemperatureData> getTemperature(@RequestBody HashMap<String, Object> body) {
-        LocalDate date = LocalDate.parse((String) body.get("date"), DateTimeFormatter.ISO_DATE);
-        Optional<TemperatureData> temperatureData = temperatureDataService.findTemperature(date);
-        if(temperatureData.isEmpty()) {
-            return (SingleResult<TemperatureData>) responseService.failResult("해당 날짜에 대한 정보가 없습니다.");
-        }
-        return responseService.singleResult(temperatureData.get());
     }
 }
